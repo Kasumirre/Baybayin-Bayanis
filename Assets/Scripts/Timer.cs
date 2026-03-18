b41be93b90b36ff4b57c0674287c44ac0cd614fc
Kasumirre <105653241+Kasumirre@users.noreply.github.com>
2026-03-18 20:07:03 +0800

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public int timeRemaining = 45;
+    public bool timerIsRunning = true;
+
+    public TextMeshProUGUI timerText;     
+
+    private float timerAccumulator = 0f;    
+
+    void Update()
+    {
+        if (!timerIsRunning) return;
+
+        timerAccumulator += Time.deltaTime;
+
+        if (timerAccumulator >= 1f)
+        {
+            timeRemaining -= 1;              
+            timerAccumulator = 0f;         
+
+            timerText.text = timeRemaining.ToString("00"); 
+
+            if (timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+
+                //Reset puzzle or trigger failure
+            }
+        }
+    }
+}
